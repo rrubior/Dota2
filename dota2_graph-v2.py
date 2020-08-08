4b112b7ed1e9225417699a4f11c708b5b573bc21
Roberto Rubio <roberto.rubio@gmail.com>
2020-08-08 12:58:52 +0200

    Added date to dictionary

--- dota2_graph-v2.py
+++ dota2_graph-v2.py
@@ -7,8 +7,9 @@ import matplotlib.pyplot as plt
 from datetime import datetime #v2.1
 
 account_id = '7000212' #enter your account ID here
+limit = 40; #enter the limit of games to analyze
 
-queries = {'limit': 40, 'lobby_type': 7} #limit is the recent number of matches it will query, lobby type 7 = ranked, other parameters can be added based on openDOTA's API
+queries = {'limit': limit, 'lobby_type': 7} #limit is the recent number of matches it will query, lobby type 7 = ranked, other parameters can be added based on openDOTA's API
 
 res = requests.get(f'https://api.opendota.com/api/players/{account_id}/Matches', params = queries)
 
@@ -16,15 +17,29 @@ data = res.json()
 
 dictionary = {} #v2.1 - Creating dictionary to save pairs of date:result
 
+# v2.1 - Convert the match timestamp to dd-mm-yy format
+def convert_date(match): #v2.1
+    tstamp = match['start_time']
+    dtime = datetime.fromtimestamp(tstamp)
+    date = dtime.strftime('%d-%m-%y')
+    # print('Fecha del partido: ', date) #v2.1 (just for debug)
+    return date
+
 def check_wins(data):
     match_results = []
     for matches in data:
+        date = convert_date(matches) #v2.1 - Get the match date in a string format
+
         if matches['radiant_win'] == True and matches['player_slot'] <=127:
             match_results.append('Win')
+            dictionary.update({'Win': date})
         elif matches['radiant_win'] == False and matches['player_slot'] >127:
             match_results.append('Win')
+            dictionary.update({'Win': date})
         else:
             match_results.append('Loss')
+            dictionary.update({'Loss': date})
+
     match_results.reverse() #reverses list to set origin from furthest back requested match
     return match_results
 
@@ -40,29 +55,11 @@ def wins_numerically(match_results): #converts list with history of win loss int
     return match_history
 
 results = wins_numerically(wins)
-
-# v2.1 - Convert the match timestamp to a dd-mm-yy format
-def convert_date(data): #v2.1
-    match_dates = []
-    for matches in data:
-        tstamp = matches['start_time']
-        dtime = datetime.fromtimestamp(tstamp)
-        date = dtime.strftime('%d-%m-%y')
-        # print('Fecha del partido: ', date) #v2.1 (just for debug)
-        return date
-
-dates = convert_date(data) #v2.1
-
-def create_dictionary(dates, wins): #v2.1
-    dict = {}
-    for win in wins:
-        dict.update({win: dates})
-    print(dict)
-
-
-create_dictionary(dates, wins)
+print(results)
 
 plt.scatter(range(len(results)), results)
 plt.plot(range(len(results)), results)
+
+plt.title('Last '+ str(limit) + ' games')
 
 plt.show()
